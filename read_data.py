6c6fc6ebbfac78c8de182ee9e59be3c605d3c982
prasanjithpatel <panduprasanjith@gmail.com>
2024-10-26 16:30:21 +0530

    convert to json

--- read_data.py
+++ read_data.py
@@ -2,11 +2,15 @@ import os
 import pandas as pd
 
 
-read = "/home/prasanjith/Desktop/atr/silver/FOREXCOM_XAGUSD_3m.csv"
-read1 = "/home/prasanjith/Desktop/atr/silver/FOREXCOM_XAGUSD_ytd.csv"
+read = "/home/prasanjith/Desktop/Atr/Atr/NSE_BANKNIFTY, 1D.csv"
+read1 = "/home/prasanjith/Desktop/Atr/Atr/NSE_NIFTY, 1D (1).csv"
+read4 = "/home/prasanjith/Desktop/Atr/Atr/NSE_NIFTYJR, 1D.csv"
 
 read2 = pd.read_csv(read)
 read3 =pd.read_csv(read1)
+read5 = pd.read_csv(read4)
 
-read2.to_json("hour_day.json", orient='records')
-read3.to_json("one_day.json",orient="records")
+
+read2.to_json("NSE_BANKNIFTY-1D", orient='records')
+read3.to_json("NSE_NIFTY-1D",orient="records")
+read5.to_json("NSE_NIFTYJR",orient="records")
